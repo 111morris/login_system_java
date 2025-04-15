aad33c49b765c338c6669a4fb360b1fcc0aad7b1
mulandi <mulandimorris1@gmail.com>
2025-04-15 20:09:41 +0300

    dvpkg

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,2 +1,40 @@
-public class LoginPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class LoginPage implements ActionListener {
+
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+
+    JTextField userIdField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+
+    JLabel userIdLabel = new JLabel("UserId: ");
+    JLabel userPasswordLabel = new JLabel("Password");
+    JLabel messageLabel = new JLabel();
+
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+
+    LoginPage(HashMap<String, String> loginInfoOriginal){
+        loginInfo = loginInfoOriginal;
+
+        userIdLabel.setBounds(50,100,70,25);
+        userPasswordLabel.setBounds(50, 150, 70, 25);
+
+
+
+        frame.add(userIdLabel);
+        frame.add(userPasswordLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
