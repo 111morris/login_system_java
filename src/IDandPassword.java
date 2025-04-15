308a801ee5bcc5c9a8103ce7e0bc8640bf9c4b45
mulandi <mulandimorris1@gmail.com>
2025-04-15 19:55:56 +0300

    dvpkg

--- src/IDandPassword.java
+++ src/IDandPassword.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class IDandPassword {
+    // hashmap stores two value pair
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+
+    IDandPassword(){
+        loginInfo.put("morris", "password123");
+        loginInfo.put("teacher", "teacher123");
+        loginInfo.put("sudent", "student123");
+
+    }
+    protected HashMap getLoginInfo(){
+        return loginInfo;
+    }
 }
